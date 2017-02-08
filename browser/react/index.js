3991cf4a3d47445730f4ba0ef6a1efc73fa5ae20
existentialtype <konstantinos.pouliasis@gmail.com>
2017-02-08 14:55:59 -0500

    update to singer

--- browser/react/index.js
+++ browser/react/index.js
@@ -1,3 +1,10 @@
 'use strict';
 
-console.log('Hello React');
+const React = require('react')
+const ReactDOM = require('react-dom')
+const Main = require('./Main')
+
+
+ReactDOM.render((<Main />),
+   document.getElementById('app')
+);
